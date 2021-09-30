e2349485b9887297e526994acadd0d441b946ad4
luisangeluis <luisangeluis@gmail.com>
2021-09-30 16:21:05 -0500

    Se obtienen los primeros 20 pokemon
    Revisar que mas se podria hacer con la api.

--- js/index.js
+++ js/index.js
@@ -1,39 +1,43 @@
 const btnGetApi = document.querySelector('#get-api');
 const divFotos20Pokemon = document.querySelector('.primeros-veinte_fotos');
 
-const getApi = async()  =>{
-   const  info  = await axios({
+const getApi = async pURL => {
+    const info = await axios({
         method: 'GET',
-        url: 'https://pokeapi.co/api/v2/pokemon'
-    })
-
+        url: pURL
+    });
     return info;
 }
-
-btnGetApi.addEventListener('click',()=>{
+//Mostrar los primeros 20 pokemon
+btnGetApi.addEventListener('click', () => {
     //Entendi que para manejo de errores con el catch se pueden manejar
-    getApi()
-        .then(res=>{
+    const divImg = document.querySelector('.primeros-veinte_fotos');
+    getApi('https://pokeapi.co/api/v2/pokemon')
+        .then(res => {
             // console.log(res)
             // console.log(res.data)
-            let pokemons = res.data.results;
+            let pokemons = res.data;
             console.log(pokemons);
 
-            pokemons.forEach(element => {
-                axios({
-                    method:'GET',
-                    url: element.url
-                })
-                .then(res=>{
-                    console.log(res);
-                })
+            const fragment = document.createDocumentFragment();
+
+            pokemons.results.forEach(element => {
+                // console.log(element);
+                const img = document.createElement('img');
+
+                getApi(element.url)
+                    .then(res => {
+                        console.log(res);
+                         img.src = res.data.sprites.front_default; 
+                         img.setAttribute('class','foto_pokemon');
+                    })
+                fragment.appendChild(img);
             });
+            divImg.appendChild(fragment);
             
-            // return res;
+
         })
-       
-        .catch(error=>{
+        .catch(error => {
             console.log(error);
         })
-
 });
